e230e16618066247ff3494e4a7df71e33e24e5bb
Arlo Wotherspoon <awotherspoon@eit.ac.nz>
2023-09-12 10:58:07 +1200

    Updated site

--- content/js/scripts.js
+++ content/js/scripts.js
@@ -5,3 +5,13 @@ function toggleContact() {
   const contactForm = document.getElementById("container");
   contactForm.style.display = "block";
 }
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
